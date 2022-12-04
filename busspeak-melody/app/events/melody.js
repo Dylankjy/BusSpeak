592f6b60e452f3f275be62399b8a6bab4251c718
Dylan Kok <me@dylankjy.com>
2022-12-04 11:08:44 +0800

    Logic for sending the play melody command to core

--- busspeak-melody/app/events/melody.js
+++ busspeak-melody/app/events/melody.js
@@ -1,11 +1,43 @@
-module.exports = (socket) => {
-    socket.on('busstop_connect', (data) => {
+module.exports = (io, socket) => {
+    socket.on('busstop_connect', async (data) => {
         // Check whether data.stopID is integer
         if (Number.isInteger(data.stopID)) {
             console.log(`BusSpeak Core at ID ${data.stopID} has connected`)
+
+            // Leave all existing rooms
+            socket.leaveAll()
+
+            // Join room with corresponding Bus Stop ID
             return socket.join(data.stopID)
         }
 
         socket.emit('req_error', { code: 'INVALID_STOPID', description: 'stopID provided is invalid.' })
+    })
+
+    socket.on('user_play_melody', (data) => {
+        /*
+        data = {
+            stopID: 1234
+            // No need to include client id as it you can just read the default room id
+        }
+        */
+
+        console.log(`User at Bus Stop ${data.stopID} has requested to play melody`)
+
+        // Check whether stop ID is integer
+        if (!Number.isInteger(data.stopID)) {
+            console.log('a')
+            return socket.emit('req_error', { code: 'INVALID_STOPID_CLIENT', description: 'stopID provided in the command is invalid.' })
+        }
+
+        // Check whether busstop is in the room
+        if (!io.sockets.adapter.rooms.has(data.stopID)) {
+            console.log('b')
+            return socket.emit('req_error', { code: 'REQUESTED_BUSCORE_NOT_FOUND', description: 'The BusSpeak Core that you are attempting to contact does not exist. stopID may be invalid.' })
+        }
+
+        // TODO: Client rate limiting
+
+        return socket.to(data.stopID).emit('busstop_play_melody', {})
     })
 }
