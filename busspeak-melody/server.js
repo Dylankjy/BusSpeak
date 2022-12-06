290a9bc43ba913b0e953150fe81ed9ee410f8453
Dylan Kok <me@dylankjy.com>
2022-12-07 00:24:05 +0800

    Added testing tool for melody

--- busspeak-melody/server.js
+++ busspeak-melody/server.js
@@ -7,8 +7,18 @@ const server = require('http').Server(app)
 const io = require('socket.io')(server)
 require('./app/events')(io)
 
+const cors = require('cors')
+const path = require('path')
+app.use(cors({
+    origin: 'http://localhost:5010'
+}))
+
 app.get('/', (req, res) => {
     return res.json({ status: 200, service_name: 'BusSpeak Melody Websocket Service', owner: 'BusSpeak' })
+})
+
+app.get('/test', (req, res) => {
+    return res.sendFile(path.join(__dirname, 'test.html'))
 })
 
 app.all('*', (req, res) => {
